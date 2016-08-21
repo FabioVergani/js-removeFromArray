3919950674a933e1226003ed63b56408e6b6febf
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-08-21 14:36:50 +0200

    Create removeFromArray.js

---
+++ removeFromArray.js
@@ -0,0 +1,7 @@
+function removeFromArray(a,b){let i=0;const n=-1,x=a,m=b;while((i=m.indexOf(x,i)) !== n){m.splice(i,1);};}
+
+/*
+let zz=["hello","hello","world",1,"world"];
+removeFromArray("hello",zz);
+console.dir(zz)
+*/
